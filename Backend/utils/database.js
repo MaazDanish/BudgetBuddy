1e107dcc450436768ea33a4741494d701bb0fa53
MaazDanish <dk599318@gmail.com>
2024-03-05 11:00:55 +0530

    changed to structureof project

--- Backend/utils/database.js
+++ Backend/utils/database.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1:27017/budgetBuddy'
+const url = 'mongodb://127.0.0.1:27017/BudgetBuddy'
 
 
 const connectToMongoDB = async () => {
     try {
         const connection = await mongoose.connect(url);
-        // console.log('database is connected successfully', connection);
+
+        
+         console.log('database is connected successfully', connection);
     } catch (er) {
         console.log(er);
     }
